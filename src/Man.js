4f3894dce65624baf95087dc9030fd47af7edcec
vamsisai843 <vamsisain64@gmail.com>
2022-06-26 20:57:42 +0530

    new

---
+++ src/Man.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Man = () => {
+  return (
+    <div>
+
+        helkglkewlkglekbglkwerglkbelkblkenbglkberr
+    </div>
+  )
+}
+
+export default Man
---
+++ src/star.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const star = () => {
+  return (
+    <div>
+
+        <h1>evnklnklenglneeg</h1>
+    </div>
+  )
+}
+
+export default star
